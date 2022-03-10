fa298b7afe2b45227729e53ff2f1831eeeedc322
iinaamasum <iinaamasum@gmail.com>
2022-03-10 23:41:40 +0600

    user__name__and__text__bug__fixed__in__comment__area

--- js/app.js
+++ js/app.js
@@ -60,6 +60,7 @@ const createPost = (post) => {
   const userImage = post.userImage;
   const div = document.createElement('article');
   div.classList.add('post');
+  console.log(post);
   div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
@@ -130,9 +131,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post?.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post?.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
